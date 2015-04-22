952443725fe1f44cf596e209532ee903525e88f8
Pierre Fourreau <fourreau.pierre@gmail.com>
2015-04-23 01:05:33 +0200

    Resolve error

---
+++ app/src/main/java/com/fourreau/itwapp/core/module/InterviewModule.java
@@ -0,0 +1,19 @@
+package com.fourreau.itwapp.core.module;
+
+import com.fourreau.itwapp.activity.LoginActivity;
+import com.fourreau.itwapp.service.AuthenticationService;
+import com.fourreau.itwapp.service.impl.AuthenticationServiceImpl;
+
+import dagger.Module;
+import dagger.Provides;
+
+/**
+* Created by Pierre on 22/04/2015.
+*/
+@Module(injects = LoginActivity.class)
+public class InterviewModule {
+    @Provides
+    public AuthenticationService providesAuthenticationService() {
+        return new AuthenticationServiceImpl();
+    }
+}
