0c1e46130dcd5253dc7656c021ea9eb9ed82175e
Pierre Fourreau <fourreau.pierre@gmail.com>
2015-05-22 16:48:26 +0200

    Login service

--- app/src/main/java/com/fourreau/itwapp/service/impl/AuthenticationServiceImpl.java
+++ app/src/main/java/com/fourreau/itwapp/service/impl/AuthenticationServiceImpl.java
@@ -1,11 +1,12 @@
 package com.fourreau.itwapp.service.impl;
 
-import com.fourreau.itwapp.model.User;
 import com.fourreau.itwapp.service.AuthenticationService;
 
 import javax.inject.Inject;
 
 import io.itwapp.Itwapp;
+import io.itwapp.exception.UnauthorizedException;
+import io.itwapp.rest.AccessToken;
 
 /**
  * Created by Pierre on 15/04/2015.
@@ -16,14 +17,21 @@ public class AuthenticationServiceImpl implements AuthenticationService{
     public AuthenticationServiceImpl() {}
 
     @Override
-    public User login(String email, String password) {
-        User user = new User();
+    public Boolean login(String email, String password) {
+//        User user = new User();
 
-
+        try {
+            AccessToken accessToken = Itwapp.Authenticate(email, password);
+            Itwapp.apiKey = accessToken.getApiKey();
+            Itwapp.secretKey = accessToken.getSecretKey();
+            return true;
+        }catch(UnauthorizedException exc)   {
+            return false;
+        }
 
         //        user = Interview.login(email, password);
 
-        return user;
+//        return user;
     }
 
 }
